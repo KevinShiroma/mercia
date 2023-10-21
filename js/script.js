347fbce4cbd50812decfa9cada7ae755d612d7d0
Kevin Shiroma <90286842+KevinShiroma@users.noreply.github.com>
2023-10-21 11:52:25 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-import DropdownMenu from "./modules/dropdown-menu.js";
+import DropdownMenu from './modules/dropdown-menu.js';
+import ativaHamburguer from './modules/hamburguer-menu.js';
 
-const dropdown = new DropdownMenu("[data-dropdown]");
+const dropdown = new DropdownMenu('[data-dropdown]');
 dropdown.init();
+ativaHamburguer();
