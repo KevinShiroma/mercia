e21ca20e36312437d00f2db765bc3d43eb8e92b2
Kevin Shiroma <90286842+KevinShiroma@users.noreply.github.com>
2023-11-08 19:37:23 -0300

    box LGPD

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-import DropdownMenu from './modules/dropdown-menu.js';
-import ativaHamburguer from './modules/hamburguer-menu.js';
+import DropdownMenu from "./modules/dropdown-menu.js";
+import ativaHamburguer from "./modules/hamburguer-menu.js";
 
-const dropdown = new DropdownMenu('[data-dropdown]');
+const dropdown = new DropdownMenu("[data-dropdown]");
 dropdown.init();
 ativaHamburguer();
+
+/* Box LGPD */
+
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => {
+  //if cookie contains codinglab it will be returned and below of this code will not run
+  if (document.cookie.includes("codinglab")) return;
+  cookieBox.classList.add("show");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+
+      //if button has acceptBtn id
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
